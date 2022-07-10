a144f17ab8f1c8a1acc0585f1e8d54ec89e97602
VishveshPatil <vishvesh2347@gmail.com>
2022-07-10 18:40:17 +0530

    change done

--- ProjectFile1.py
+++ ProjectFile1.py
@@ -2,3 +2,9 @@ A = 51
 B = 31
 C = A+B
 print("Addtion of given two numbers: ",A,"and",B,"=",C)
+
+print("Another program of  multiplication is  added later on... ")
+X = 10
+Y = 10
+Z = X * Y
+print("multiplication of ",X,"and",Y,"=",Z)
